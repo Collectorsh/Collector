50dd127b8e5a6ec722e1250a9297934e4ac9d791
Richard Fisher <richardfsr@gmail.com>
2022-06-17 21:49:01 +0800

    update featured

--- components/home/Featured.jsx
+++ components/home/Featured.jsx
@@ -34,82 +34,88 @@ export default function Featured() {
       <h1 className="text-4xl font-extrabold text-black dark:text-whitish text-center pt-8">
         Featured
       </h1>
+      <p className="text-center mt-4">
+        Curated by{" "}
+        <Link href="https://twitter.com/CompoundI3">
+          <a className="underline">Compound</a>
+        </Link>
+      </p>
       <div className="bg-black dark:bg-black mt-8 pb-4 dark:border-t dark:border-b dark:border-dark3 mb-12">
         <div className="bg-black dark:bg-black pt-8">
           <Slider {...settings}>
             <div className="px-4 text-center align-center">
-              <Link href={`/CultureHacker`}>
+              <Link href="https://exchange.art/artists/Sleepr/nfts">
                 <a>
                   <Image
                     token={{
                       image:
-                        "https://cdn.collector.sh/43EX7Fyve884g9miSrW9fGNHhca5bwkzJJiGEG9sY5mr",
+                        "https://images-cdn.exchange.art/ipfs/bafybeihl5eww7ubuvn3yequtoly3gyc4f5aanc3beoqllrwjbvfkw3gyqa?ext=jpg&quality=100&width=1000&dpr=1",
                     }}
                   />
                 </a>
               </Link>
               <p className="text-gray-100 mt-2 text-sm">
-                @CultureHacker {"//"} Collector
+                @sleeprNFT {"//"} Exchange.Art
               </p>
             </div>
             <div className="px-4 text-center align-center">
-              <Link href="https://formfunction.xyz/@ilanderech/9Q4rdiTyQgfabRvCBdug1wdNYY3USt6okCbaGSYsL6JN">
+              <Link href="https://exchange.art/artists/J.T.%20Liss/nfts">
                 <a>
                   <Image
                     token={{
                       image:
-                        "https://cdn.collector.sh/9Q4rdiTyQgfabRvCBdug1wdNYY3USt6okCbaGSYsL6JN",
+                        "https://images-cdn.exchange.art/ku5m_lPhkaLm195euLpnVR0Hwd0vJg6co_zd4LJ1LDo?ext=jpg?ext=jpg&quality=100&width=1000&dpr=1",
                     }}
                   />
                 </a>
               </Link>
               <p className="text-gray-100 mt-2 text-sm">
-                @IDerech {"//"} Formfunction
+                @JTLissPhotoArt {"//"} Exchange.Art
               </p>
             </div>
             <div className="px-4 text-center align-center">
-              <Link href="https://exchange.art/artists/douglas%20/nfts">
+              <Link href="https://formfunction.xyz/@laurenceantony">
                 <a>
                   <Image
                     token={{
                       image:
-                        "https://images-cdn.exchange.art/zfx9cVDB4T1dqhvefDjNCwWZ5XQjy0pIvxLrVCtaViM?ext=jpg?ext=jpg&quality=100&width=1000&dpr=1",
+                        "https://cdn.collector.sh/72kPXftajfA8sfs9XM6aDCUXDMPwZxisBvYK7SaUaxf1",
                     }}
                   />
                 </a>
               </Link>
               <p className="text-gray-100 mt-2 text-sm">
-                @Douglas {"//"} Exchange.Art
+                @laurence_antony {"//"} Formfunction
               </p>
             </div>
             <div className="px-4 text-center align-center">
-              <Link href="https://formfunction.xyz/@CryptoVulture">
+              <Link href="https://exchange.art/artists/Degen%20Poet/nfts">
                 <a>
                   <Image
                     token={{
                       image:
-                        "https://cdn.collector.sh/84fdcc3s3xnUZw2F6kgUeGhnL6gPLANHRQBWJJ4duEfh",
+                        "https://collector.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F786a758e-309d-4673-887f-f018a851ddb5%2FUntitled.png?table=block&id=e09a011f-0f94-4d35-9608-01ab6f924b01&spaceId=914cb796-030c-4559-93ed-49b454f68b6b&width=1060&userId=&cache=v2",
                     }}
                   />
                 </a>
               </Link>
               <p className="text-gray-100 mt-2 text-sm">
-                @CryptoVulture {"//"} Formfunction
+                @degenpoet {"//"} Exchange.Art
               </p>
             </div>
             <div className="px-4 text-center align-center">
-              <Link href="https://exchange.art/series/Analog%20Nights/nfts">
+              <Link href="https://formfunction.xyz/@kiramoto">
                 <a>
                   <Image
                     token={{
                       image:
-                        "https://images-cdn.exchange.art/VZ8FvgkhG4TewQs35Hyaufp3s9OseQ9RDAfDzujOC44?ext=jpg?ext=jpg&quality=100&width=1000&dpr=1",
+                        "https://collector.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F3d86abc7-015e-4f48-977c-26cebfa608b1%2FUntitled.png?table=block&id=06c128d5-465b-4c3b-b625-056e89df329f&spaceId=914cb796-030c-4559-93ed-49b454f68b6b&width=1150&userId=&cache=v2",
                     }}
                   />
                 </a>
               </Link>
               <p className="text-gray-100 mt-2 text-sm">
-                @TrevElViz {"//"} Exchange.Art
+                @kiramotosan {"//"} Formfunction
               </p>
             </div>
           </Slider>

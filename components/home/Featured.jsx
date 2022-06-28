15be9d5c34a8df540d899c67761264e7f83fcc65
Richard Fisher <richardfsr@gmail.com>
2022-06-28 19:44:45 +0800

    update featured

--- components/home/Featured.jsx
+++ components/home/Featured.jsx
@@ -36,85 +36,98 @@ export default function Featured() {
         </h1>
         <p className="text-whitish text-center mt-3">
           Curated by{" "}
-          <Link href="https://twitter.com/CompoundI3">
-            <a className="underline">Compound</a>
+          <Link href="https://twitter.com/artsnip3r">
+            <a className="underline">Charty</a>
           </Link>
         </p>
         <div className="bg-black dark:bg-black pt-8">
           <Slider {...settings}>
             <div className="px-4 text-center align-center">
-              <Link href="https://exchange.art/artists/Sleepr/nfts">
+              <Link href="https://twitter.com/post_screw">
                 <a>
                   <Image
                     token={{
                       image:
-                        "https://images-cdn.exchange.art/ipfs/bafybeihl5eww7ubuvn3yequtoly3gyc4f5aanc3beoqllrwjbvfkw3gyqa?ext=jpg&quality=100&width=1000&dpr=1",
+                        "https://images-cdn.exchange.art/jXuJfVd85FMFoT60192n-c2KGDkDROVho76xm9mSz4g?ext=mp4?ext=mp4",
                     }}
                   />
                 </a>
               </Link>
               <p className="text-gray-100 mt-2 text-sm">
-                @sleeprNFT {"//"} Exchange.Art
+                @Arseniy Valter {"//"} Be3sis
               </p>
             </div>
             <div className="px-4 text-center align-center">
-              <Link href="https://exchange.art/artists/J.T.%20Liss/nfts">
+              <Link href="https://twitter.com/3Pscrgg2e">
                 <a>
                   <Image
                     token={{
                       image:
-                        "https://images-cdn.exchange.art/ku5m_lPhkaLm195euLpnVR0Hwd0vJg6co_zd4LJ1LDo?ext=jpg?ext=jpg&quality=100&width=1000&dpr=1",
+                        "https://images-cdn.exchange.art/ipfs/QmYErTjgq4Fv3vohtRNECT6dFRTZFTPw2Lu7U4HDvgFmBs?quality=100&width=1000&dpr=1",
                     }}
                   />
                 </a>
               </Link>
               <p className="text-gray-100 mt-2 text-sm">
-                @JTLissPhotoArt {"//"} Exchange.Art
+                @Scrog {"//"} User 64
               </p>
             </div>
             <div className="px-4 text-center align-center">
-              <Link href="https://formfunction.xyz/@laurenceantony">
+              <Link href="https://twitter.com/youthcloud1">
                 <a>
                   <Image
                     token={{
                       image:
-                        "https://cdn.collector.sh/72kPXftajfA8sfs9XM6aDCUXDMPwZxisBvYK7SaUaxf1",
+                        "https://images-cdn.exchange.art/ipfs/bafybeifhvjfcvmfwmee4ubc2xnq6x3ixtpoipoxxgrt4k57njnbbhs6sym",
                     }}
                   />
                 </a>
               </Link>
               <p className="text-gray-100 mt-2 text-sm">
-                @laurence_antony {"//"} Formfunction
+                @Youthcloud {"//"} The last of sakura
               </p>
             </div>
             <div className="px-4 text-center align-center">
-              <Link href="https://exchange.art/artists/Degen%20Poet/nfts">
+              <Link href="https://twitter.com/belikehats">
                 <a>
                   <Image
                     token={{
                       image:
-                        "https://collector.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F786a758e-309d-4673-887f-f018a851ddb5%2FUntitled.png?table=block&id=e09a011f-0f94-4d35-9608-01ab6f924b01&spaceId=914cb796-030c-4559-93ed-49b454f68b6b&width=1060&userId=&cache=v2",
+                        "https://collector.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fd9e93279-488e-456f-ab19-262c99db1823%2FUntitled.png?table=block&id=c481c5e6-dbae-4a62-a263-9b095fba8c47&spaceId=914cb796-030c-4559-93ed-49b454f68b6b&width=960&userId=&cache=v2",
+                    }}
+                  />
+                </a>
+              </Link>
+              <p className="text-gray-100 mt-2 text-sm">@HATS {"//"} Magnus</p>
+            </div>
+            <div className="px-4 text-center align-center">
+              <Link href="https://twitter.com/superjimmer">
+                <a>
+                  <Image
+                    token={{
+                      image:
+                        "https://collector.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F07ae4768-d7fe-48ca-abf1-9e1ab5ac6a88%2FUntitled.png?table=block&id=92b70f49-bb01-40b9-8bf4-23b035829f17&spaceId=914cb796-030c-4559-93ed-49b454f68b6b&width=1150&userId=&cache=v2",
                     }}
                   />
                 </a>
               </Link>
               <p className="text-gray-100 mt-2 text-sm">
-                @degenpoet {"//"} Exchange.Art
+                @Jim Roll {"//"} In the shadow of the gods
               </p>
             </div>
             <div className="px-4 text-center align-center">
-              <Link href="https://formfunction.xyz/@kiramoto">
+              <Link href="https://twitter.com/boywithnolegs">
                 <a>
                   <Image
                     token={{
                       image:
-                        "https://collector.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F3d86abc7-015e-4f48-977c-26cebfa608b1%2FUntitled.png?table=block&id=06c128d5-465b-4c3b-b625-056e89df329f&spaceId=914cb796-030c-4559-93ed-49b454f68b6b&width=1150&userId=&cache=v2",
+                        "https://collector.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fe667d114-e7c6-4a04-b273-a5a3e13dd3e0%2FUntitled.png?table=block&id=5e8cc3d6-3296-43fa-8979-e09b4932acf0&spaceId=914cb796-030c-4559-93ed-49b454f68b6b&width=1920&userId=&cache=v2",
                     }}
                   />
                 </a>
               </Link>
               <p className="text-gray-100 mt-2 text-sm">
-                @kiramotosan {"//"} Formfunction
+                @NOLEGS {"//"} Trapped In Torment
               </p>
             </div>
           </Slider>

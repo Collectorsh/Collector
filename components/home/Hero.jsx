6d6d1b2e234ae10d410e5a076c0ced8055330a53
Richard Fisher <richardfsr@gmail.com>
2022-07-02 16:24:04 +0800

    place nav below the fold

--- components/home/Hero.jsx
+++ components/home/Hero.jsx
@@ -17,9 +17,9 @@ export default function Hero() {
 
   return (
     <>
-      <div className="mx-auto px-2 md:px-4 lg:px-12 pb-0 lg:pb-4 bg-white dark:bg-black mt-28">
+      <div className="mx-auto px-2 md:px-4 lg:px-12 pb-0 lg:pb-4 bg-white dark:bg-black mt-[100px] min-h-[calc(100vh-100px)]">
         <div className="mx-auto px-2 md:px-0">
-          <div className="lg:grid lg:grid-cols-7 lg:gap-x-8 mb-8 lg:mb-0">
+          <div className="lg:grid lg:grid-cols-7 lg:gap-x-8 mb-8 lg:mb-0 items-center">
             <div className="col-span-4 align-middle xl:mr-20 lg:mt-12 mb-6">
               <h1 className="font-bold tracking-wide text-black dark:text-whitish text-center">
                 <p className="text-5xl">Discover</p>

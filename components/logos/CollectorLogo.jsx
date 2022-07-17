aa917d1cba1cdebaf542fac73e388c9676e418ac
Richard Fisher <richardfsr@gmail.com>
2022-07-17 18:50:49 +0800

    fix text color again

--- components/logos/CollectorLogo.jsx
+++ components/logos/CollectorLogo.jsx
@@ -1,7 +1,10 @@
 export default function CollectorLogo({ color }) {
   return (
-    <span className="font-sans text-sm">
-      <div className="w-3 h-3 rounded-full bg-greeny inline-block align-middle mr-1 -mt-0.5 text-black dark:text-white"></div>
+    <span
+      className="font-sans text-sm text-black dark:text-white"
+      style={{ color: color }}
+    >
+      <div className="w-3 h-3 rounded-full bg-greeny inline-block align-middle mr-1 -mt-0.5"></div>
       Collector
     </span>
   );

34483ea2a1739c9287df3b9ff6df81e5731a21e7
Richard Fisher <richardfsr@gmail.com>
2022-07-24 13:19:38 +0800

    send extra data to estimate query

--- data/getEstimatedValue.js
+++ data/getEstimatedValue.js
@@ -1,9 +1,30 @@
 import apiClient from "/data/client/apiClient";
+import { rpcHost } from "/config/settings";
+import { Connection } from "@metaplex/js";
+import { PublicKey } from "@solana/web3.js";
+import { Metadata } from "@metaplex-foundation/mpl-token-metadata";
 
 async function getEstimatedValue(token) {
+  const connection = new Connection(rpcHost);
+  try {
+    var tokenEditiondata = await Metadata.getEdition(
+      connection,
+      new PublicKey(token.mint)
+    );
+  } catch (err) {
+    console.log(err);
+  }
+
+  var editionKey = tokenEditiondata ? tokenEditiondata.data.key : null;
+
   const res = await apiClient.post(
     "/estimate/price",
-    { mint: token.mint },
+    {
+      mint: token.mint,
+      creator: token.creator,
+      artist_name: token.artist_name,
+      edition: editionKey,
+    },
     {
       "Content-Type": "application/json",
       Accept: "application/json",

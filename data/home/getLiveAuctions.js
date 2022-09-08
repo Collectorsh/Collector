6a97003b1191d712f2a77a7b9d5b00252a385166
Richard Fisher <richardfsr@gmail.com>
2022-09-08 16:38:17 +0100

    remove log

--- data/home/getLiveAuctions.js
+++ data/home/getLiveAuctions.js
@@ -3,7 +3,6 @@ import apiClient from "/data/client/apiClient";
 async function getLiveAuctions() {
   try {
     const res = await apiClient.post("/auctions/live");
-    console.log(res)
     return res;
   } catch (error) {
     console.log(error);

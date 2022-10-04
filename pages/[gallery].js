c1b499f62313c7bb7ecad8b4391c5ffd4f4e996f
Richard Fisher <richardfsr@gmail.com>
2022-10-04 21:10:50 +0100

    remove logging

--- pages/[gallery].js
+++ pages/[gallery].js
@@ -69,9 +69,7 @@ export async function getServerSideProps(context) {
     let username = context.params.gallery;
     let res = await getUserFromUsername(username);
     let user = res.user;
-    console.log(user);
     let tokens = await getMetadata(user.public_keys);
-    console.log(tokens);
     return { props: { user, tokens } };
   } catch (err) {
     console.log(err);

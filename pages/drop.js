ccef9a439cf9985188a2404c84d964d98c1cff9e
Richard Fisher <richardfsr@gmail.com>
2022-11-22 19:54:27 +0000

    checkpoint

--- pages/drop.js
+++ pages/drop.js
@@ -1,40 +1,75 @@
-import { useContext, useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import MainNavigation from "/components/navigation/MainNavigation";
 import { Metaplex, walletAdapterIdentity } from "@metaplex-foundation/js";
 import { Connection, clusterApiUrl } from "@solana/web3.js";
 import { PublicKey } from "@solana/web3.js";
 import { useWallet } from "@solana/wallet-adapter-react";
-import {
-  useWalletModal,
-} from "@solana/wallet-adapter-react-ui";
+import { useWalletModal } from "@solana/wallet-adapter-react-ui";
 
 export default function DropHome() {
-  const { wallet, publicKey } = useWallet();
+  const wallet = useWallet();
   const { setVisible } = useWalletModal();
-  const [holder, setHoler] = useState(false);
+  const [holder, setHolder] = useState();
+  const [total, setTotal] = useState(0);
+  const [remaining, setRemaining] = useState();
+  const [collectionMint, setCollectionMint] = useState();
+
+  const connection = new Connection(
+    clusterApiUrl(process.env.NEXT_PUBLIC_SOLANA_NETWORK)
+  );
+
+  const metaplex = new Metaplex(connection).use(walletAdapterIdentity(wallet));
+
+  const asyncGetCandymachine = useCallback(async (wallet, onceOnly = false) => {
+    const cndy = await metaplex.candyMachines().findByAddress({
+      address: new PublicKey(process.env.NEXT_PUBLIC_DROP_CANDYMACHINE),
+    });
+    setTotal(cndy.itemsLoaded);
+    setRemaining(cndy.itemsRemaining.toNumber());
+    if (onceOnly === false) setTimeout(asyncGetCandymachine, 5000);
+  }, []);
+
+  const asyncCheckIfHolder = useCallback(async (wallet) => {
+    const nfts = await metaplex.nfts().findAllByOwner({
+      owner: wallet.publicKey,
+    });
+    var holder = false;
+    for (const nft of nfts) {
+      if (
+        nft.collection &&
+        nft.collection.address.toBase58() ===
+          process.env.NEXT_PUBLIC_SIGNATURE_COLLECTION_ADDRESS
+      ) {
+        setCollectionMint(nft);
+        setHolder("yes");
+        holder = true;
+      }
+    }
+    if (holder === false) setHolder("no");
+  }, []);
+
+  useEffect(() => {
+    if (!wallet || !wallet.publicKey) return;
+    asyncGetCandymachine(wallet);
+    asyncCheckIfHolder(wallet);
+  }, [wallet]);
 
   const mintNow = async () => {
-    const collectionUpdateAuthority = new PublicKey(
-      process.env.NEXT_PUBLIC_UPDATE_AUTHORITY
-    );
-    const connection = new Connection(clusterApiUrl(process.env.NEXT_PUBLIC_SOLANA_NETWORK));
-    const metaplex = new Metaplex(connection).use(
-      walletAdapterIdentity(wallet)
-    );
     const candyMachine = await metaplex.candyMachines().findByAddress({
       address: new PublicKey(process.env.NEXT_PUBLIC_DROP_CANDYMACHINE),
     });
-    console.log(candyMachine);
+    const collectionUpdateAuthority = candyMachine.authorityAddress;
     const { nft } = await metaplex.candyMachines().mint({
       candyMachine,
       collectionUpdateAuthority,
       guards: {
         nftGate: {
-          mint: new PublicKey("8xro5M8Lz7VBAZCwSKu84uLgzDiRjYYBn5ptE7gTrVXD"),
+          mint: collectionMint.mintAddress,
         },
       },
     });
     console.log(nft);
+    asyncGetCandymachine(wallet, true);
   };
 
   return (
@@ -43,74 +78,71 @@ export default function DropHome() {
         <MainNavigation />
         <div className="max-w-7xl mx-auto">
           <div className="max-w-4xl mx-auto mt-12 p-4 shadow-lg bg-white">
-            <img src="/images/gacha.jpg" className="inline w-16 h-16 align-middle" />
+            <img
+              src="/images/gacha.jpg"
+              className="inline w-16 h-16 align-middle"
+            />
             <h2 className="align-middle inline my-5 text-4xl font-bold text-gray-800 w-full py-1 inline-block">
               Collector Gacha
             </h2>
 
-            <p className="mt-4">Multiple artists, multiple editions, which one will you mint?</p>
+            <p className="mt-4">
+              Multiple artists, multiple editions, which one will you mint?
+            </p>
 
-            <p className="mt-4">The Collector Gacha is a fun way to mint art from artists that you know and some that you don&apos;t.  All artists share equally in the sales and collectors can try their luck to see what they will get.</p>
+            <p className="mt-4">
+              The Collector Gacha is a fun way to mint art from artists that you
+              know and some that you don&apos;t. All artists share equally in
+              the sales and collectors can try their luck to see what they will
+              get.
+            </p>
 
-            <p className="mt-6 bg-gray-100 p-2 w-ft font-bold">Remaining:  30/30</p>
-
-            <p className="mt-4 bg-gray-100 p-2 w-ft font-bold">Price:  2 SOL</p>
-
-            {!publicKey &&
-              <button
-                className="mt-12 bg-greeny px-3 py-2 font-semibold text-black text-xl cursor-pointer"
-                onClick={(e) => setVisible(true)}
-              >
-                Connect Wallet
-              </button>
-            }
-
-            {publicKey && !holder &&
-              <p className="mt-12 text-red-500">You need to be a Collector Signature holder in order to participate.  You can mint yours <a href="/mint" target="_blank" className="font-bold underline">here</a></p>
-            }
-
-          </div>
-          {/* <div className="grid md:grid-cols-2 md:gap-10 mt-10">
-            <div className="grid md:grid-cols-2 md:gap-2 dark:bg-dark3 bg-gray-50 p-2 rounded-lg">
-              <div>
-                <img
-                  className="rounded-lg mb-2"
-                  src="https://arweave.net/ny32VOK2vWQkaNFbcw7IIEfFTnrpH5paYOsbIIR98ic?ext=png"
-                />
-                <img
-                  className="rounded-lg"
-                  src="https://arweave.net/gzLfBofb7jLjJS-MeNI2e7Tv2NYtCKj4GB8xS6d_hoA?ext=png"
-                />
-              </div>
-              <div>
-                <img
-                  className="rounded-lg"
-                  src="https://arweave.net/ny32VOK2vWQkaNFbcw7IIEfFTnrpH5paYOsbIIR98ic?ext=png"
-                />
-                <h1 className="text-7xl text-center mt-16">?</h1>
-              </div>
-            </div>
-            <div className="relative mb-10 dark:text-whitish">
-              <h1 className="text-3xl font-bold">3 Artists</h1>
-              <h2 className="text-lg">10 editions each</h2>
-              <h1 className="text-3xl font-bold mt-6">
-                Which one will you get?
-              </h1>
-              <p className="mb-2 text-red-600 text-sm">
-                Connect your wallet to continue
+            {remaining && total && (
+              <p className="mt-6 bg-gray-100 p-2 w-ft font-bold">
+                Remaining: {remaining}/{total}
               </p>
-              <div className="absolute top-12"></div>
-              <p className="text-left text-3xl font-bold mb-5 mt-12"></p>
-              <p className="text-left text-2xl font-bold"></p>
+            )}
+
+            <p className="mt-4 bg-gray-100 p-2 w-ft font-bold">Price: 1 SOL</p>
+
+            {!wallet ||
+              (!wallet.publicKey && (
+                <button
+                  className="mt-12 bg-greeny px-3 py-2 font-semibold text-black text-xl cursor-pointer"
+                  onClick={(e) => setVisible(true)}
+                >
+                  Connect Wallet
+                </button>
+              ))}
+
+            {wallet &&
+              wallet.publicKey &&
+              holder === "yes" &&
+              remaining === 0 && (
+                <button className="mt-12 bg-red-400 px-3 py-2 font-semibold text-black text-xl">
+                  Sold Out
+                </button>
+              )}
+
+            {wallet && wallet.publicKey && holder === "yes" && remaining > 0 && (
               <button
-                className="rounded bg-gray-300 px-1 py-0.5 mt-1 font-semibold text-black text-xs cursor-pointer"
-                onClick={mintNow}
+                className="mt-8 bg-greeny px-3 py-2 font-semibold text-black text-xl cursor-pointer"
+                onClick={(e) => mintNow()}
               >
                 Mint
               </button>
-              <p className="text-left text-lg font-bold mb-3">Description</p>
-            </div>
-          </div> */}
+            )}
+
+            {wallet && wallet.publicKey && holder === "no" && (
+              <p className="mt-12 text-red-500">
+                You need to be a Collector Signature holder in order to
+                participate. You can mint yours{" "}
+                <a href="/mint" target="_blank" className="font-bold underline">
+                  here
+                </a>
+              </p>
+            )}
+          </div>
         </div>
       </div>
     </>

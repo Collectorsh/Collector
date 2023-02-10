d560ae44bacbd371fac157aaf9947f458b402b2e
Richard Fisher <richardfsr@gmail.com>
2023-02-10 13:21:02 +0000

    allow ctrl click

--- pages/drops/[name]/nft.js
+++ pages/drops/[name]/nft.js
@@ -27,33 +27,35 @@ export default function Nft({ mint }) {
   return (
     <>
       {token && (
-        <Link href={link}>
-          <div className="relative bg-white dark:bg-dark3 shadow-lg sm:shadow-xl rounded-2xl pt-[10px] px-[10px] border border-gray-200 dark:border-dark3">
-            <div className="rounded-lg overflow-hidden">
-              <Image token={token} />
-            </div>
-            <div>
-              <div className="w-full">
-                <div className="h-12 my-1">
-                  <h3 className="text-md text-black dark:text-whitish font-medium">
-                    {token.name}
-                  </h3>
-                  {token.attributes && token.attributes.length > 0 && (
-                    <p className="dark:text-whitish text-xs">
-                      {token.attributes[0].value}
-                    </p>
-                  )}
-                  {mint.listed && (
-                    <div className="float-right mb-2">
-                      <MarketplaceLogo source={mint.source} color="white" /> ◎
-                      {roundToTwo(mint.amount / 1000000000)}
-                    </div>
-                  )}
-                </div>
+        <div className="relative bg-white dark:bg-dark3 shadow-lg sm:shadow-xl rounded-2xl pt-[10px] px-[10px] border border-gray-200 dark:border-dark3">
+          <div className="rounded-lg overflow-hidden">
+            <Link href={link}>
+              <a>
+                <Image token={token} />
+              </a>
+            </Link>
+          </div>
+          <div>
+            <div className="w-full">
+              <div className="h-12 my-1">
+                <h3 className="text-md text-black dark:text-whitish font-medium">
+                  {token.name}
+                </h3>
+                {token.attributes && token.attributes.length > 0 && (
+                  <p className="dark:text-whitish text-xs">
+                    {token.attributes[0].value}
+                  </p>
+                )}
+                {mint.listed && (
+                  <div className="float-right mb-2">
+                    <MarketplaceLogo source={mint.source} color="white" /> ◎
+                    {roundToTwo(mint.amount / 1000000000)}
+                  </div>
+                )}
               </div>
             </div>
           </div>
-        </Link>
+        </div>
       )}
     </>
   );

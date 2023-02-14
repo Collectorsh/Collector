83918545ce8c3a8452358cd2cf23c2476f716b53
Richard Fisher <richardfsr@gmail.com>
2023-02-14 16:03:06 +0000

    use the same rpc for wallet and mint

--- contexts/wallet.js
+++ contexts/wallet.js
@@ -21,7 +21,9 @@ export const WalletContextProvider = ({ children }) => {
   const network = process.env.NEXT_PUBLIC_SOLANA_NETWORK;
 
   // You can also provide a custom RPC endpoint
-  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  // const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const endpoint = process.env.NEXT_PUBLIC_SOLANA_GACHA_RPC;
 
   // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
   // Only the wallets you configure here will be compiled into your application, and only the dependencies

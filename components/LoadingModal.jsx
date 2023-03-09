2de86f96d60db0faa615ba5b9fc43640948cd1ea
Richard Fisher <richardfsr@gmail.com>
2023-03-09 11:04:45 +0000

    fix loading modal

--- components/LoadingModal.jsx
+++ components/LoadingModal.jsx
@@ -43,7 +43,7 @@ export default function LoadingModal({ open, title, content, icon }) {
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
             <div className="relative inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+              <div className="bg-white dark:bg-dark3 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                 <div className="sm:flex sm:items-start">
                   <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10">
                     {icon === "download" && (
@@ -62,22 +62,24 @@ export default function LoadingModal({ open, title, content, icon }) {
                   <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                     <Dialog.Title
                       as="h3"
-                      className="text-lg leading-6 font-medium text-gray-900"
+                      className="text-lg leading-6 font-medium dark:text-white text-gray-900"
                     >
                       {title}
                     </Dialog.Title>
                     <div className="mt-2">
-                      <p className="text-sm text-gray-500">{content}</p>
-                    </div>
-                    <div className="mt-4 w-[50px] mx-auto h-64">
-                      <Oval
-                        color="#fff"
-                        secondaryColor="#000"
-                        height={50}
-                        width={50}
-                      />
+                      <p className="text-sm text-gray-500 dark:text-whitish">
+                        {content}
+                      </p>
                     </div>
                   </div>
+                </div>
+                <div className="pt-16 w-[50px] mx-auto h-64">
+                  <Oval
+                    color="#fff"
+                    secondaryColor="#000"
+                    height={50}
+                    width={50}
+                  />
                 </div>
               </div>
             </div>

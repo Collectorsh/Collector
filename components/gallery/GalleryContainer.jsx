da7eba6ae5097264b8ec6c244405cb324cbb32f7
Richard Fisher <richardfsr@gmail.com>
2023-04-14 17:14:52 +0100

    increase gap

--- components/gallery/GalleryContainer.jsx
+++ components/gallery/GalleryContainer.jsx
@@ -12,7 +12,7 @@ export default function GalleryContainer({ tokens, user }) {
     <div className="clear-both w-full mt-6">
       <div className="clear-both">
         <div
-          className={`grid grid-cols-1 sm:grid-cols-${columns} gap-x-0 gap-y-8 sm:gap-8 items-center pb-12`}
+          className={`grid grid-cols-1 sm:grid-cols-${columns} gap-x-0 gap-y-8 sm:gap-8 md:gap-12 lg:gap-16 items-center pb-12`}
         >
           {Array.isArray(tokens) &&
             tokens.map((token, index) => {

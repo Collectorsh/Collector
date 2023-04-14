b162e87e691dc5c594edc6dc07d4a4b613a80a42
Richard Fisher <richardfsr@gmail.com>
2023-04-14 17:04:39 +0100

    use grid layout and detect landscape and use colspan-2

--- components/gallery/Card.jsx
+++ components/gallery/Card.jsx
@@ -4,11 +4,16 @@ import Listings from "/components/gallery/Listings";
 import ShowOffers from "/components/gallery/ShowOffers";
 import Nft from "/components/gallery/Nft";
 import MetaContainer from "/components/gallery/MetaContainer";
+import { useImageSize } from "react-image-size";
 
 function Card(props) {
+  const image = props.token.image;
   const user = props.user;
   const isMounted = useRef(true);
   const [token, setToken] = useState();
+  const [span, setSpan] = useState(1);
+
+  const [dimensions, { loading, error }] = useImageSize(image);
 
   const initGetData = useCallback(async (tok) => {
     try {
@@ -26,8 +31,15 @@ function Card(props) {
     };
   }, [props.token, initGetData]);
 
+  useEffect(() => {
+    if (!dimensions) return;
+    if (dimensions.width / dimensions.height > 1.25) {
+      setSpan(2);
+    }
+  }, [dimensions]);
+
   return (
-    <div className="opacity-0 relative text-center">
+    <div className={`col-span-${span} relative text-center h-fit`}>
       {token && (
         <>
           <Nft user={user} token={token} />
--- components/gallery/GalleryContainer.jsx
+++ components/gallery/GalleryContainer.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import Card from "/components/gallery/Card";
-import Masonry from "react-masonry-css";
 
 export default function GalleryContainer({ tokens, user }) {
   useEffect(() => {
@@ -9,30 +8,18 @@ export default function GalleryContainer({ tokens, user }) {
 
   const columns = user && user.columns ? user.columns : 3;
 
-  const breakpointColumnsObj = {
-    default: columns,
-    1100: columns - 1,
-    700: columns - 2,
-  };
-
   return (
     <div className="clear-both w-full mt-6">
       <div className="clear-both">
-        <Masonry
-          breakpointCols={breakpointColumnsObj}
-          className={`masonry-grid ${columns === 2 && "-ml-24"} ${
-            columns === 3 && "-ml-16"
-          } ${columns === 4 && "-ml-10"} ${columns === 5 && "-ml-8"}`}
-          columnClassName={`masonry-grid_column ${columns === 2 && "pl-24"} ${
-            columns === 3 && "pl-16"
-          } ${columns === 4 && "pl-10"} ${columns === 5 && "pl-8"}`}
+        <div
+          className={`grid grid-cols-1 sm:grid-cols-${columns} gap-x-0 gap-y-8 sm:gap-8 items-center pb-12`}
         >
           {Array.isArray(tokens) &&
             tokens.map((token, index) => {
               if (token.visible)
                 return <Card key={index} token={token} user={user} />;
             })}
-        </Masonry>
+        </div>
       </div>
     </div>
   );

a83fd82b85fc6e41f3555e41976c3728f788135f
Scott Harlan <56773004+SeHarlan@users.noreply.github.com>
2023-06-07 12:42:44 -0700

    no directory link

--- components/navigation/MainNavigation.jsx
+++ components/navigation/MainNavigation.jsx
@@ -69,11 +69,14 @@ export default function MainNavigation() {
                 <Link href="/shop">Shop</Link>
               </p> */}
                 {/* {!user && <Premium />} */}
-                <Link href="/galleries">
+
+
+
+                {/* <Link href="/galleries">
                   <a className="mr-8 font-bold">
                     Directory
                   </a>
-                </Link>
+                </Link> */}
               <Link href="/about">
                 <a className="mr-8 font-bold">
                   About
@@ -178,7 +181,13 @@ export default function MainNavigation() {
                               }`}
                           >
                             <Link href="/">Home</Link>
-                          </p>
+                              </p>
+                              {/* <p
+                                className={`text-xl cursor-pointer border-b-2 py-2 border-gray-100 dark:border-dark3 ${ path === "/galleries" ? "font-bold" : ""
+                                  }`}
+                              >
+                                <Link href="/galleries">Directory<Link>
+                              </p> */}
                             {/* {!user && (
                               <>
                                 <p className="text-xl font-light cursor-pointer border-b-2 border-gray-100 dark:border-dark3 py-2">

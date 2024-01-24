350e826760320272e8bfd4269196b78e85479935
Scott Harlan <56773004+SeHarlan@users.noreply.github.com>
2024-01-24 10:55:17 -0800

    username error handle

--- components/create/mintModal.jsx
+++ components/create/mintModal.jsx
@@ -283,7 +283,8 @@ const MintModal = ({ nftProps, isOpen, onClose, onReset }) => {
       case MINT_STAGE.ERROR: return (
         <div className="h-56 flex flex-col gap-2 justify-center">
           <p className="text-center text-2xl font-bold">Sorry, there was an error minting your art onchain.</p>
-          <p className="text-center">Please, make sure your internet connection is stable and then try again.</p>
+          
+          {/* <p className="text-center">Please, make sure your internet connection is stable and then try again.</p> */}
         </div>
       )
       
